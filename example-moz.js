cc35d96ba9ba8e45c92481b9ea6e1147d66b40fb
Mike Davies <isofarro2@gmail.com>
2015-10-18 10:01:03 +0100

    example of moz.

---
+++ example-moz.js
@@ -0,0 +1,12 @@
+var fs = require('fs'),
+    moz = require('./MozChecker');
+
+var DOMAIN_FILE = 'domain_list.txt',
+    reader = fs.createReadStream(DOMAIN_FILE, {encoding: 'utf-8'}),
+    mozChecker = moz.MozChecker();
+
+// reader.on('data', function (chunk) {
+//     console.log(chunk);
+// });
+
+reader.pipe(mozChecker);
