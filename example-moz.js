2a6d2463a26899685a18eca675f40208f4d0e24a
Mike Davies <isofarro2@gmail.com>
2015-10-18 11:29:34 +0100

    Remove commented out code

--- example-moz.js
+++ example-moz.js
@@ -5,10 +5,6 @@ var DOMAIN_FILE = 'domain_list.txt',
     reader = fs.createReadStream(DOMAIN_FILE, {encoding: 'utf-8'}),
     mozChecker = moz.MozChecker();
 
-// reader.on('data', function (chunk) {
-//     console.log(chunk);
-// });
-
 reader
     .pipe(mozChecker)
     .pipe(process.stdout);
