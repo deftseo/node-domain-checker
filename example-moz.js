49202a9467ff67f7513bac8595e6d927ef092540
Isofarro <isofarro2@gmail.com>
2016-06-23 12:09:41 +0100

    fix logic error in params

--- example-moz.js
+++ example-moz.js
@@ -2,7 +2,7 @@ var fs = require('fs'),
     moz = require('./MozChecker');
 
 var DOMAIN_FILE = 'domain_list.txt',
-    file = (process.argv.length > 1) ? process.argv[2] : DOMAIN_FILE
+    file = (process.argv.length > 2) ? process.argv[2] : DOMAIN_FILE,
     reader = fs.createReadStream(file, {encoding: 'utf-8'}),
     mozChecker = moz.MozChecker();
 
